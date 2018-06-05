d8d2c352dbf51cba5c82a37b155c1b52589c8816
evo-li <1174024506@qq.com>
2018-06-05 08:02:57 +0800

    remove pidfile cofig

--- deployfiles/gunicorn.conf.py
+++ deployfiles/gunicorn.conf.py
@@ -6,7 +6,11 @@ import multiprocessing
 bind = 'unix:/tmp/iterate.site.socket'
 workers = multiprocessing.cpu_count() * 2 + 1
 # daemon = True
-pidfile = '/run/gunicorn.pid'
+
+# if pidfile set,pid file would be created,
+# when deploy ,Error: Already running on PID 2771 (or pid file '/run/gunicorn.pid' is stale)
+#pidfile = '/run/gunicorn.pid'
+
 loglevel = 'info'
 errorlog = '/home/evo/sites/iterate.site/blogproject/logfiles/gunicorn_error.log'
 accesslog = '/home/evo/sites/iterate.site/blogproject/logfiles/gunicorn_access.log'
