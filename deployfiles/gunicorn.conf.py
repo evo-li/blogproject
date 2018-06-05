5612aa948a3992ebbf7190e9ae1137f1f57ed5c8
evo-li <1174024506@qq.com>
2018-06-05 08:31:59 +0800

    change log

--- deployfiles/gunicorn.conf.py
+++ deployfiles/gunicorn.conf.py
@@ -12,6 +12,6 @@ workers = multiprocessing.cpu_count() * 2 + 1
 #pidfile = '/run/gunicorn.pid'
 
 loglevel = 'info'
-errorlog = '/home/evo/sites/iterate.site/blogproject/logfiles/gunicorn_error.log'
-accesslog = '/home/evo/sites/iterate.site/blogproject/logfiles/gunicorn_access.log'
+#errorlog = '/home/evo/sites/iterate.site/blogproject/logfiles/gunicorn_error.log'
+#accesslog = '/home/evo/sites/iterate.site/blogproject/logfiles/gunicorn_access.log'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
